1579400bd0a71abdd787835cc6b16d201db1980a
jjhwan-h <jjhwan.h@gamil.com>
2024-07-14 13:47:01 +0900

    feat:Initialize rest utils package

---
+++ rest/rest.go
@@ -0,0 +1,105 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/jjhwan-h/CBNU_DIDI_BLOCKCHAIN/blockchain"
+	"github.com/jjhwan-h/CBNU_DIDI_BLOCKCHAIN/utils"
+)
+
+var port string
+
+type url string
+
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+type urlDescription struct {
+	URL         url    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+type addBlockBody struct {
+	Data string
+}
+
+type errorResponse struct {
+	ErrorMessage string `json:"errorMessage"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription{
+		{
+			URL:         url("/"),
+			Description: "See Documentation",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "POST",
+			Description: "Add a block",
+			Payload:     "data:string",
+		},
+		{
+			URL:         url("/blocks/{height}"),
+			Method:      "GET",
+			Description: "See a Block",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+	json.NewEncoder(rw).Encode(data)
+}
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		//{"data":"block data"}
+		var addBlockBody addBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Data)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func block(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["height"])
+	utils.HandleErr(err)
+	block, err := blockchain.GetBlockchain().GetBlock(id)
+	encoder := json.NewEncoder(rw)
+	if err == blockchain.ErrNotFound {
+		encoder.Encode(errorResponse{fmt.Sprint(err)})
+	} else {
+		encoder.Encode(block)
+	}
+}
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
+}
+
+func Start(aPort int) {
+	router := mux.NewRouter() // url 과 url 함수를 이어주는 역할
+	port = fmt.Sprintf(":%d", aPort)
+	router.Use(jsonContentTypeMiddleware)
+	router.HandleFunc("/", documentation).Methods("GET")
+	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
+	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
